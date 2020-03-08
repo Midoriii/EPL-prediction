f390f69349c1782d32cd5f25dc64c87d3ea65e7c
Štěpán Beneš <stephen.Team24@gmail.com>
2020-03-08 22:06:46 +0100

    Simple check for team names and match number per season.

--- data_prep.py
+++ data_prep.py
@@ -6,5 +6,26 @@ import glob
 import json
 import os
 
-season_stats1415 = json.load(open('data/season14-15/season_stats.json','r'))
-print("{}".format(json.dumps(season_stats1415,indent=2))[:3000])
+match_stats1415 = json.load(open('data/season14-15/season_match_stats.json','r'))
+match_stats1516 = json.load(open('data/season15-16/season_match_stats.json','r'))
+match_stats1617 = json.load(open('data/season16-17/season_match_stats.json','r'))
+match_stats1718 = json.load(open('data/season17-18/season_match_stats.json','r'))
+
+#Load simple match stats from each season
+matches1415 = pd.DataFrame.from_dict(match_stats1415, orient='index')
+matches1516 = pd.DataFrame.from_dict(match_stats1516, orient='index')
+matches1617 = pd.DataFrame.from_dict(match_stats1617, orient='index')
+matches1718 = pd.DataFrame.from_dict(match_stats1718, orient='index')
+
+all_seasons_matches = [matches1415, matches1516, matches1617, matches1718]
+
+for matches in all_seasons_matches:
+    print(matches.shape)
+
+#Get unique team names per season
+teams_per_season = []
+for matches in all_seasons_matches:
+    teams_per_season.append(matches.home_team_name.unique())
+
+for teams in teams_per_season:
+    print(teams)
