b410977b6fc5d3f7003e66994846aac25abe4a1f
Midoriii <stephen.Team24@gmail.com>
2020-04-24 22:49:46 +0200

    Refactored the data generating script, added diff series lengths data

--- rnn_data_prep.py
+++ rnn_data_prep.py
@@ -16,6 +16,13 @@ def get_team_matches(season, team_id):
 # Main fuction for now
 if __name__== "__main__":
 
+    # How many matches in a series ?
+    if len(sys.argv) > 1:
+        series_length = int(sys.argv[1])
+    # If not specified, just do 5
+    else:
+        series_length = 3
+
     # Since we don't care about recent form when using RNNs, any of the normalized datasets is fine
     # Normalized should also work better with nets
     data_normalized = pd.DataFrame()
@@ -46,17 +53,18 @@ if __name__== "__main__":
         away_team_matches_before = get_team_matches(data_normalized.iloc[0:i], away_team)
 
         # If there are enough matches played to form a series, form a series !
-        if len(home_team_matches_before) > 3:
-            # Append the last 4 matches as a series
-            Series_list = Series_list.append(home_team_matches_before[-4:])
+        if len(home_team_matches_before) >= series_length:
+            # Append the last series_length matches as a series
+            Series_list = Series_list.append(home_team_matches_before[- series_length:])
             # Append the result of this match as a label for prediction
             labels.append(data_normalized.iloc[i]['result_home'])
             #print(Series_list)
             #print(len(labels))
+            #print(data_normalized.iloc[i]['match_id'])
 
         # Do the same for the away team
-        if len(away_team_matches_before) > 3:
-            Series_list = Series_list.append(away_team_matches_before[-4:])
+        if len(away_team_matches_before) >= series_length:
+            Series_list = Series_list.append(away_team_matches_before[- series_length:])
             labels.append(data_normalized.iloc[i]['result_home'])
 
     # We don't need match ids for predictions and also the index values
@@ -65,11 +73,11 @@ if __name__== "__main__":
 
     # Create data and labels as numpy arrays and reshape accordingly
     Y = np.array(labels)
-    #print(Y.shape)
+    print(Y.shape)
     X = Series_list.to_numpy()
-    X = X.reshape(2856,4,59)
-    #print(X.shape)
+    X = X.reshape(int(len(Series_list)/series_length),series_length,59)
+    print(X.shape)
 
     # Save for further usage
-    np.save('rnn_data/labels', Y)
-    np.save('rnn_data/data', X)
+    np.save('rnn_data/labels_' + str(series_length), Y)
+    np.save('rnn_data/data_' + str(series_length), X)
