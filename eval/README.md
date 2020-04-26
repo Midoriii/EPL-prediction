783ca4aca0858a5dbadc62a05a4571c14ae3baeb
Midoriii <stephen.Team24@gmail.com>
2020-04-26 22:34:35 +0200

    Added series of length 8, updated readme, more rnns

--- eval/README.md
+++ eval/README.md
@@ -1,6 +1,6 @@
-** results.csv ** contains the sorted models by their achieved accuracy over 100 training and testing trials on randomly selected data splits (85/15 train/test)
+**results.csv** contains the sorted models by their achieved accuracy over 100 training and testing trials on randomly selected data splits (85/15 train/test)
 
-** end_of_season_results.csv ** contains the same but the testing set is made of the last 50 matches of each season
+**end_of_season_results.csv** contains the same but the testing set is made of the last 50 matches of each season
 
 
 The difference is that in the first case, the randomness of the split might include matches from the beginning of the season, for which my method based on
@@ -9,14 +9,14 @@ This somewhat reflects on the results, as end of the season predictions score a 
 
 It can also be seen that the end of the season predictions favour recent form span over 5 to 8 matches, while randomly selected matches lean towards more recent form of 3 matches.
 
-Their ** final ** variants mean that those were used for the final report.
+Their **final** variants mean that those were used for the final report.
 
 
-** grid_results.csv ** contains the best results of differently parameterized forests, which showed to be the best classifiers, through grid search
+**grid_results.csv** contains the best results of differently parameterized forests, which showed to be the best classifiers, through grid search
 
-** grid_results_params.csv ** holds the best configuration of forests for each dataset
+**grid_results_params.csv** holds the best configuration of forests for each dataset
 
-** grid_results_combined_top_15 ** provides an easy to read overview of the best params for the top 15 scoring models
+**grid_results_combined_top_15** provides an easy to read overview of the best params for the top 15 scoring models
 
 
 Turns out that parameters matter very little with the forests. There are dependencies like max_features and number_of_estimators (lolw -> low, high -> high), but overall there is no superior combination of params.
--- rnn_test.py
+++ rnn_test.py
@@ -21,10 +21,14 @@ if __name__== "__main__":
     batch_s = 8
     n_outputs = 3
 
-    scores = []
-    accs = []
+    scores_1 = []
+    scores_2 = []
+    scores_3 = []
+    accs_1 = []
+    accs_2 = []
+    accs_3 = []
 
-    for series_length in [3,4,5]:
+    for series_length in [3,4,5,8]:
 
         data = np.load('rnn_data/data_' + str(series_length) + '.npy')
         labels = np.load('rnn_data/labels_' + str(series_length) + '.npy')
@@ -51,20 +55,50 @@ if __name__== "__main__":
         # Simple model
         model_1 = Sequential()
         model_1.add(LSTM(128, input_shape=(series_length, 59), dropout=0.4, recurrent_dropout=0.4))
-        #model.add(Dropout(0.5))
         model_1.add(Dense(n_outputs, activation='softmax'))
         model_1.compile(optimizer='adam', loss='categorical_crossentropy',metrics=['accuracy'] )
 
+        # Deeper model
+        model_2 = Sequential()
+        model_2.add(LSTM(150, input_shape=(series_length, 59), return_sequences=True, dropout=0.4, recurrent_dropout=0.4))
+        model_2.add(LSTM(100, return_sequences=True, dropout=0.4, recurrent_dropout=0.4))
+        model_2.add(LSTM(50, return_sequences=True, dropout=0.4, recurrent_dropout=0.4))
+        model_2.add(LSTM(25, dropout=0.4, recurrent_dropout=0.4))
+        model_2.add(Dense(n_outputs, activation='softmax'))
+        model_2.compile(optimizer='adam', loss='categorical_crossentropy',metrics=['accuracy'] )
+
+        # Bidirectional model
+        model_3 = Sequential()
+        model_3.add(Bidirectional(LSTM(128, dropout=0.4, recurrent_dropout=0.4), input_shape=(series_length, 59)))
+        #model.add(Dropout(0.5))
+        model_3.add(Dense(n_outputs, activation='softmax'))
+        model_3.compile(optimizer='adam', loss='categorical_crossentropy',metrics=['accuracy'] )
+
         print(model_1.summary())
+        print(model_2.summary())
+        print(model_3.summary())
 
         # Train for a few epochs
-        history = model_1.fit(data_train, labels_train, batch_size=batch_s, epochs=100, validation_split=0.2, verbose=1)
+        history_1 = model_1.fit(data_train, labels_train, batch_size=batch_s, epochs=50, validation_split=0.2, verbose=1)
+        history_2 = model_2.fit(data_train, labels_train, batch_size=batch_s, epochs=50, validation_split=0.2, verbose=1)
+        history_3 = model_3.fit(data_train, labels_train, batch_size=batch_s, epochs=50, validation_split=0.2, verbose=1)
 
         # Finally make a prediction
-        score, acc = model_1.evaluate(data_test, labels_test, batch_size=8)
+        score, acc = model_1.evaluate(data_test, labels_test, batch_size=batch_s)
+        scores_1.append(score)
+        accs_1.append(acc)
 
-        scores.append(score)
-        accs.append(acc)
+        score, acc = model_2.evaluate(data_test, labels_test, batch_size=batch_s)
+        scores_2.append(score)
+        accs_2.append(acc)
 
-    print(scores)
-    print(accs)
+        score, acc = model_3.evaluate(data_test, labels_test, batch_size=batch_s)
+        scores_3.append(score)
+        accs_3.append(acc)
+
+    print("Model 1 score: " + str(scores_1) + "\n" +
+          "Model 2 score: " + str(scores_2) + "\n" +
+          "Model 3 score: " + str(scores_3))
+    print("Model 1 acc: " + str(accs_1) + "\n" +
+          "Model 2 acc: " + str(accs_2) + "\n" +
+          "Model 3 acc: " + str(accs_3))
